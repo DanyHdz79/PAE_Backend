9158a1c809414d242263ede2bae727cd94857b5b
AaronCG01 <A01730451@itesm.mx>
2022-06-13 14:25:41 -0500

    Settings changed for external server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sistemapae',
-        'USER': 'dhzsql',
-        'PASSWORD': 'sqlpass',
+        'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
